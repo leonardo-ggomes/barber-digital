898a45314d14bbdca72eb89370010d661300544b
Leonardo Gomes Garcia <Leonardo Gomes Garcia>
2026-05-12 16:18:23 -0300

    Projeto: Add 3D

--- aplicativo_web/server.js
+++ aplicativo_web/server.js
@@ -14,9 +14,11 @@ class Server
         this.port = port
         this.app.use(express.json())
         this.app.use(express.urlencoded({extended: true}))
-        this.app.use(router)
         this.app.set("view engine", "ejs")
         this.app.set("views","mvc/views")
+        this.app.use(express.static("mvc/views/static"))
+        this.app.use(router)
+
     }
 
     listen()
